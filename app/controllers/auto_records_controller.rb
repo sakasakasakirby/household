a9339391ac27a6f409c473b9c306bc0822ba065b
sakasakasakirby <1180329z@gmail.com>
2020-11-12 17:56:20 +0900

    added delete auto_record function

--- app/controllers/auto_records_controller.rb
+++ app/controllers/auto_records_controller.rb
@@ -1,5 +1,5 @@
 class AutoRecordsController < ApplicationController
-  before_action :before_method
+  before_action :before_method, only: [:new, :create]
 
   def new
     @autoRecord = AutoRecord.new
@@ -9,7 +9,7 @@ class AutoRecordsController < ApplicationController
     @autoRecord = AutoRecord.new(autoRecord_params)
     name_permit()
     if @autoRecord.save
-      redirect_to root_path
+      redirect_to auto_record_path(current_user)
     else
       if @autoRecord[:name] == nil
         flash.now[:alert] = "項目名に空白が含まれています。空白を除いて入力してください。"
@@ -17,6 +17,29 @@ class AutoRecordsController < ApplicationController
         flash.now[:alert] = "金額に数値以外、もしくは0以下1,000,000,000以上の値が入力されています。1~999,999,999の範囲の整数値を入力してください。"
       end
       render :new
+    end
+  end
+
+  def show
+    @autoRecord = current_user.auto_records
+  end
+
+  def destroy
+    array = params.keys
+    check_id = []
+    array.each do |i|
+      if i.to_i != 0
+        check_id.push(i.to_i)
+      end
+    end
+    if check_id.length != 0
+      check_id.each do |i|
+        AutoRecord.find(i).destroy
+      end
+      redirect_to auto_record_path(current_user)
+    else
+      @autoRecord = current_user.auto_records
+      render :show
     end
   end
   
